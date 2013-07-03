1e964d7fcf57fca2a323d7ea92353bbf01eb2eb8
Oleg Sedletskyi <frank.rtf@gmail.com>
2013-07-03 17:17:42 +0300

    added request to mysql

--- webapp/DemoTomcat/src/com/boinic/tomcat/DemoServlet.java
+++ webapp/DemoTomcat/src/com/boinic/tomcat/DemoServlet.java
@@ -1,4 +1,5 @@
 package com.boinic.tomcat;
+import java.sql.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,7 +16,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/DemoServlet")
 public class DemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+			 
+	    public String databaseCall() {
+	        Connection con = null;
+	        Statement st = null;
+	        ResultSet rs = null;
+	        String myVersion = "";
+	 
+	        //String url = "jdbc:mysql://127.3.224.130:3306/java"; //make sure that this database name exists;
+	        String url = "JunkDB:mysql://127.3.96.130:3306"; //make sure that this database name exists;
+	        //mysql://adminx8mVKcQ:G4xbfTrTbrEZ@127.3.96.130:3306
+	        String user = "admin";
+	        String password = "G4xbfTrTbrEZ";
+	 
+	        try {
+	            try {
+	                Class.forName("com.mysql.jdbc.Driver");
+	            } catch (ClassNotFoundException cnfe) {
+	                return cnfe.getMessage();
+	            }
+	            con = DriverManager.getConnection(url, user, password);
+	            st = con.createStatement();
+	            rs = st.executeQuery("SELECT * from users;");
+	 
+	            if (rs.next()) {
+	                System.out.println(rs.getString(1));
+	                myVersion = rs.getString(1);
+	            }
+	 
+	        } catch (SQLException ex) {
+	            myVersion = ex.getMessage();
+	        } finally {
+	            try {
+	                if (rs != null) {
+	                    rs.close();
+	                }
+	                if (st != null) {
+	                    st.close();
+	                }
+	                if (con != null) {
+	                    con.close();
+	                }
+	 
+	            } catch (SQLException ex) {
+	                myVersion = ex.getMessage();
+	            }
+	        }
+	        return myVersion;
+	    }
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -31,6 +80,8 @@ public class DemoServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
+		databaseCall();
+		out.println("After DB query");
 	}
 
 	/**
